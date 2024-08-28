e17705b8714711c4a8db43d652df4bf64daecdb8
T-H-Badhon <thbadhons@gmail.com>
2024-08-28 15:05:04 +0600

    1

--- mycode.js
+++ mycode.js
@@ -1,1 +1,22 @@
-console.log("HI")
+
+
+
+var createCounter = function(n) {
+    let count=-1
+    return function(){
+        count++
+
+        return n+count
+    }
+};
+
+const counter= createCounter(10)
+
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+
+
